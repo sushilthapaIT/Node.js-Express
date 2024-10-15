de68d9b3794fc96122ce38e9fb87fe06d9eda2d7
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-10-15 03:52:36 -0400

    Created Error Handle Middleware

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
@@ -6,8 +7,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json()); 
-
 app.use("/api/contacts", require("./routes/contactRoute"));
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
