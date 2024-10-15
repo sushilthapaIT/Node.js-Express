4a0dbe4ba13751868a9140907fb89c3ac6247da6
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-10-15 01:55:25 -0400

    added controller for all the request and response ()
    Created contact controllers for contact CRUD

---
+++ mycontacts-backend/controllers/contactController.js
@@ -0,0 +1,41 @@
+//@desc Get all contacts
+//@route GET /api/contacts
+//@access public 
+
+const getContact = (req, res) => { //api call
+    res.status(200).json({message: "Get all contacts"})
+};
+
+//@desc Create New Contacts
+//@route Post /api/contacts
+//@access public 
+
+const createContact = (req, res) => { //to create
+    res.status(201).json({message: "Create Contact"})
+};
+
+//@desc Get Contacts
+//@route GET /api/contacts
+//@access public 
+
+const getContacts = (req, res) => { //route for individual contact
+    res.status(200).json({message: `Get contacts for ${req.params.id}`})
+};
+
+//@desc Update New Contacts
+//@route PUT /api/contacts:id
+//@access public 
+
+const updateContact = (req, res) => { //to update
+    res.status(200).json({message: `Update contacts for ${req.params.id}`})
+};
+
+//@desc Delete Contacts
+//@route Delete /api/contacts:id
+//@access public 
+
+const deleteContact = (req, res) => { //to update
+    res.status(200).json({message: `Update contacts for ${req.params.id}`})
+};
+
+module.exports = {getContacts, createContact, getContact, updateContact, deleteContact};
