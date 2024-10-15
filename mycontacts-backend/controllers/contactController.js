9f2bfceac7d1cb1ba7916e04b5defd1c58a9c019
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-10-15 04:00:25 -0400

    added asyncHandler
    added asyncHandler package we do not have to write try catch block to handle error when we use this package

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -1,14 +1,16 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public 
-const getContact = async (req, res) => { //api call
+const getContact = asyncHandler((req, res) => { //api call
     res.status(200).json({message: "Get all contacts"})
-};
+});
 
 //@desc Create New Contacts
 //@route Post /api/contacts
 //@access public 
-const createContact = async (req, res) => { //to create
+const createContact = asyncHandler((req, res) => { //to create
     console.log("The request body is" ,req.body);
     const {name, email, number} = req.body;
     if(!name || !email || !number){
@@ -16,27 +18,27 @@ const createContact = async (req, res) => { //to create
         throw new Error("All fields are mandatory.")
     }
     res.status(201).json({message: "Create Contact"})
-};
+});
 
 //@desc Get Contacts
 //@route GET /api/contacts
 //@access public 
-const getContacts = async (req, res) => { //route for individual contact
+const getContacts = asyncHandler((req, res) => { //route for individual contact
     res.status(200).json({message: `Get contacts for ${req.params.id}`})
-};
+});
 
 //@desc Update New Contacts
 //@route PUT /api/contacts:id
 //@access public 
-const updateContact = async (req, res) => { //to update
+const updateContact = asyncHandler((req, res) => { //to update
     res.status(200).json({message: `Update contacts for ${req.params.id}`})
-};
+});
 
 //@desc Delete Contacts
 //@route Delete /api/contacts:id
 //@access public 
-const deleteContact = async (req, res) => { //to update
+const deleteContact = asyncHandler((req, res) => { //to update
     res.status(200).json({message: `Update contacts for ${req.params.id}`})
-};
+});
 
 module.exports = {getContacts, createContact, getContact, updateContact, deleteContact};
