739f886c326cfd7fa58edfce99920670aa0d919e
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-10-24 02:04:21 -0400

    added jwt access token for login

--- mycontacts-backend/controllers/userController.js
+++ mycontacts-backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 //@desc Register a user
@@ -39,7 +40,29 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/register
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Login user" });
+    const {email, password} = request.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory!")
+    }
+    const user = await User.findOne({email});
+    //comparing password with hashedpassword
+    if(user &&(await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            },
+        }, 
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: "1m"} //expiry time of a token
+    );
+        res.status(200).json({accessToken}); //providing access token
+    }else{
+       res.status(401) 
+       throw new Error("Email or Password is not valid");
+    }
 });
 
 //@desc Current user information
